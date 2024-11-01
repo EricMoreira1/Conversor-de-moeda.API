92917fd5f626eb32662fc1e887c8092d6f40baa2
Eric Mendes Moreira <eric.moreira@alunoescolassociais.org.br>
2024-11-01 12:18:35 -0300

    Adiciona a documentação README e finaliza os códigos do JS

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Conversor-de-moeda-API
 
 ## Descrição
-è uma versão superior e melhorada do [conversor de moedas] `https://github.com/EricMoreira1/Conversor.git`<br>
+è uma versão superior e melhorada do [conversor de moedas](https://github.com/EricMoreira1/Conversor.git)<br>
 (projeto anterior), que agora utiliza o qual converte moedas de outros<br>
 países para saber o seu valor final ao trocar de região.
+
+# Códigos e funções no JS
 
 ## $
 Permite adicionar uma várialvel a uma URL no JS
@@ -18,3 +20,25 @@ utilizado para se escrever uma URL.
 ## data
 É o que guarda tudo que está na URL como um banco de dados<br>
  sendo a abreviação de dataBase: que a base de dados.
+
+## getElementById
+Seleciona uma função utilaizando pelo Id inserido nos parentesis<br>
+
+## await
+Utilizado para esperar um valor a ser enviado dentro de uma função aynsc<br>
+
+## Fontes Utilizadas
+[Mozzila](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById/)
+
+## Tecnologias utilizadas 
+*`HTML5`<br>
+*`CSS3`<br>
+*`Git`<br>
+*`Github`<br>
+*`Javascript`<br>
+
+## Ferramentas utilizadas 
+* `VScode` 
+
+## Perfil do meu Professor
+Professor [Leonardo](https://github.com/leonardorochamarista)
--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const apiKey = '17084ec6bb48396404463f04';
 const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
 
 // Função para buscar taxa de câmbio da API
-async function getExchangeRAte(daMoeda, paraMoeda){
+async function getExchangeRate(moedaOrigem, moedaDestino){
     try{
-        const response = await fetch(`${apiURL}${daMoeda}`);
+        const response = await fetch(`${apiURL}${moedaOrigem}`);
         const data = await response.json();
 
         if(data.result === 'success'){
-            return data.conversion_rates[paraMoeda];
+            return data.conversion_rates[moedaDestino];
             }else{
                 throw new Error('Erro ao buscar a taxa de câmbio');
             }
@@ -23,6 +23,21 @@ document.getElementById('conversor-Form').addEventListener('submit', async funct
     event.preventDefault();
 
     const valor = parseFloat(document.getElementById('valor').value);
-    const daMoeda = document.getElementById('daMoeda').value;
-    const paraMoeda = document.getElementById('paraMoeda').value;
+    const moedaOrigem = document.getElementById('moedaOrigem').value;
+    const moedaDestino = document.getElementById('moedaDestino').value;
+    
+    // Função para buscar câmbio de APi
+    const exchangeRate = await getExchangeRate(moedaOrigem, moedaDestino);
+
+    if(exchangeRate){
+        const convertedValue = valor * exchangeRate;
+        //Exibir o resultado
+        const conversao = document.getElementById('conversao');
+        conversao.textContent = `Resultado: ${convertedValue.toFixed(2)} ${moedaDestino}`;
+
+    }else{
+        alert("Erro ao buscar a cotação informada. Tente novamente")
+    }
 });
+
+
